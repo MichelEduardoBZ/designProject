425b05c74dc7c8f8476eba18de1a2c3bb83ca925
MichelEduardoBZ <michel.zeschau@gmail.com>
2025-05-08 21:29:20 -0300

    Project created

---
+++ backend/src/main/java/br/com/testDesign/TestDesignApplication.java
@@ -0,0 +1,13 @@
+package br.com.testDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDesignApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/testDesign/TestDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
