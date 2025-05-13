86ef299db7f30f1b0b2a8fc217fdeb6b5e54a667
MichelEduardoBZ <michel.zeschau@gmail.com>
2025-05-13 20:44:07 -0300

    Created CRUD for category.

--- backend/src/main/java/br/com/testDesign/resources/CategoryResource.java
+++ backend/src/main/java/br/com/testDesign/resources/CategoryResource.java
@@ -3,12 +3,14 @@ package br.com.testDesign.resources;
 import br.com.testDesign.dto.CategoryDTO;
 import br.com.testDesign.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -18,7 +20,41 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        return ResponseEntity.ok(categoryService.findAll());
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "DESC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return ResponseEntity.ok(categoryService.findAll(pageRequest));
+    }
+
+    @GetMapping(value = "/{categoryId}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable  Long categoryId) {
+        return ResponseEntity.ok(categoryService.findByIdl(categoryId));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insertCategory(@RequestBody  CategoryDTO  categoryDTO) {
+        categoryDTO = categoryService.insertCategory(categoryDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(categoryDTO.getId())
+                            .toUri();
+
+        return ResponseEntity.created(uri).body(categoryDTO);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId,  @RequestBody  CategoryDTO  categoryDTO) {
+        return ResponseEntity.ok(categoryService.updateCategory(categoryId, categoryDTO));
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId) {
+        categoryService.deleteCategory(categoryId);
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/src/main/java/br/com/testDesign/services/CategoryService.java
+++ backend/src/main/java/br/com/testDesign/services/CategoryService.java
@@ -1,15 +1,18 @@
 package br.com.testDesign.services;
 
 import br.com.testDesign.dto.CategoryDTO;
-import br.com.testDesign.entities.Category;
+import br.com.testDesign.entities.CategoryEntity;
 import br.com.testDesign.repositories.CategoryRepository;
+import br.com.testDesign.services.exceptions.DatabaseException;
+import br.com.testDesign.services.exceptions.EntityNotFoundException;
+import br.com.testDesign.transform.category.CategoryTransform;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -17,9 +20,45 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private CategoryTransform categoryTransform;
+
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll() {
-        return categoryRepository.findAll().stream().map(CategoryDTO::new).collect(Collectors.toList());
+    public Page<CategoryDTO> findAll(PageRequest pageRequest) {
+        return categoryRepository.findAll(pageRequest).map(e -> categoryTransform.transformToDTO(e));
     }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findByIdl(Long id) {
+        return categoryTransform.transformToDTO(categoryRepository.findById(id).orElseThrow(EntityNotFoundException::new));
+    }
+
+    @Transactional
+    public CategoryDTO insertCategory(CategoryDTO categoryDTO) {
+        CategoryEntity categoryEntity = categoryRepository.save(categoryTransform.transformToEntity(categoryDTO));
+        return categoryTransform.transformToDTO(categoryEntity);
+    }
+
+    @Transactional
+    public CategoryDTO updateCategory(Long categoryId, CategoryDTO categoryDTO) {
+        try {
+            CategoryEntity categoryEntity = categoryRepository.getReferenceById(categoryId);
+            categoryEntity.setName(categoryDTO.getName());
+            return categoryTransform.transformToDTO(categoryRepository.save(categoryEntity));
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Id not found " + categoryId);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void deleteCategory(Long categoryId) {
+        if(!categoryRepository.existsById(categoryId)) {
+            throw new EntityNotFoundException();
+        }
+        try {
+            categoryRepository.deleteById(categoryId);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
 }
